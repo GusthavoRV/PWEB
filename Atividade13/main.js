e62c665728de84bd1304457d9b4fa488cf922902
Gusthavo Rangel <72213417+GusthavoRV@users.noreply.github.com>
2023-05-13 18:06:26 -0300

    Add files via upload

---
+++ Atividade13/main.js
@@ -0,0 +1,11 @@
+function tudoMaius(){
+    document.getElementById("texto").value = document.getElementById("texto").value.toUpperCase();
+    document.forms.teste.opcao1.checked = true;
+    document.forms.teste.opcao2.checked = false;
+}
+
+function tudoMinus(){
+    document.getElementById("texto").value = document.getElementById("texto").value.toLowerCase();
+    document.forms.teste.opcao1.checked = false;
+    document.forms.teste.opcao2.checked = true;
+}
